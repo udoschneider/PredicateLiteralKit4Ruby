c33821d0cd36f2e8542f9ad5a9b25bce1cd864a9
Udo Schneider <Udo.Schneider@homeaddress.de>
2013-10-17 06:38:49 +0200

    Initial commit

---
+++ lib/PredicateLiteralKit4Ruby.rb
@@ -0,0 +1,5 @@
+require "PredicateLiteralKit4Ruby/version"
+
+module PredicateLiteralKit4Ruby
+  # Your code goes here...
+end
---
+++ lib/PredicateLiteralKit4Ruby/version.rb
@@ -0,0 +1,3 @@
+module PredicateLiteralKit4Ruby
+  VERSION = "0.0.1"
+end
